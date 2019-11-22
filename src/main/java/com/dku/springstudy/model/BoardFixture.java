b52440e21a9e1e89fd38b71c9e2284725b51001e
OhDonggeon <cafa3@naver.com>
2019-11-22 17:47:40 +0900

    code review added to BoardFixture.java

--- src/main/java/com/dku/springstudy/model/BoardFixture.java
+++ src/main/java/com/dku/springstudy/model/BoardFixture.java
@@ -4,6 +4,8 @@ import org.assertj.core.util.Lists;
 
 import java.util.List;
 
+//BoardRepository라고 명시해야 한다.
+//또한 @Repository 및 @Autowired 하여 사용한다.
 public class BoardFixture {
 
     public static List<Board> boardList = Lists.newArrayList(
